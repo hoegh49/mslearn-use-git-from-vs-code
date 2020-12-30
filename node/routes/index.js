1342209bfb21100aa58a14dd2db7d05ea937a3c9
PJ <hoegh49@gmail.com>
2020-12-30 22:49:52 +0100

    hgfhgf

--- node/routes/index.js
+++ node/routes/index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home pagess. */
+/* GET the home pagess. */
 router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
